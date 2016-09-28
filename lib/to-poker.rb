95f84ca915d722f118de5f8476875d94224382b5
Gregor MacDougall <gregor@freerunningtechnologies.com>
2016-09-28 11:06:53 -0400

    Initial Commit

---
+++ lib/to-poker.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Baron Top Level Library
+module ToPoker
+end
+
+require 'to-poker/card'
+require 'to-poker/deck'
+require 'to-poker/hand'
+require 'to-poker/suit'
